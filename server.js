1e5ca8ed9cbc0cec90b135570bb99054fbe81c60
Błażej K. <s9029@pjwstk.edu.pl>
2013-06-17 14:55:00 +0200

    oczyszczenie kodu

---
+++ server.js
@@ -0,0 +1,172 @@
+/*jshint node: true */
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    io = require('socket.io');
+
+   
+
+var server = http.createServer(function (req, res) {
+    'use strict';
+    var filePath = '.' + req.url,
+        contentType = 'text/html',
+        extName;
+
+    console.log('request starting...' + filePath);
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+    extName = path.extname(filePath);
+    switch (extName) {
+    case '.js':
+        contentType = 'text/javascript';
+        break;
+    case '.css':
+        contentType = 'text/css';
+        break;
+    }
+
+    path.exists(filePath, function (exists) {
+        if (exists) {
+            fs.readFile(filePath, function (error, content) {
+                if (error) {
+                    res.writeHead(500);
+                    res.end();
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    res.end(content, 'utf-8');
+                }
+            });
+        } else {
+            res.writeHead(404);
+            res.end();
+        }
+    });
+});
+
+var socket = io.listen(server);
+
+/*
+
+    5 - statków:
+        1 - 5 masztów
+        1 - 4 maszty
+        1 - 3 maszty
+        1 - 2 maszty
+
+    LEGENDA
+
+    0 - puste pole
+    1 - statek
+    2 - pudlo
+    3 - trafione
+
+
+ */
+
+ var users = {},
+    room = 'room';
+    var tab1 = [
+        [1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1]
+    ],
+    tab2 = [
+        [0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 0, 0, 0, 0, 1]
+    ],
+    players = [];
+    var amountClients = 0,
+    trafionePlayer1 = 0,
+    trafionePlayer2 = 0,
+    trafiony = 0;
+
+socket.configure('development', function (){
+    socket.set('log level', 0);
+});
+
+socket.on('connection', function (client) {
+    'use strict';
+    var username;
+    amountClients++;
+
+    console.log("Rozmiar tablicy: " + players.length);
+
+    players.push(client);
+
+    if(Object.keys(users).length===2){
+        client.emit('nieGra', 'Nie mozesz sie podlaczyc - jest wystarczajaco graczy');
+    }else{
+        client.on('setNick', function(nickname){
+            users[nickname]=nickname;
+            client.username = nickname;
+            // console.log("Gracz I: " + players[0].id);
+            // console.log("Gracz II: " + players[1]);
+            
+            if(amountClients==2){
+                players[1].emit('twojaTablica', {'statki' : tab2, 'rival' : players[0].username, 'status' : 1});
+                players[0].emit('twojaTablica', {'statki' : tab1, 'rival' : players[1].username, 'status' : 0});
+
+            }
+            if(amountClients<2){
+                client.emit('waitForPlayer', 'poczekaj na drugiego gracza');
+            }
+        });
+    }
+
+    client.on('shot', function(msg){
+        if(this===players[0]){
+            if(tab2[msg.x][msg.y]===1){
+                client.emit('dupa', {x: msg.x, y: msg.y, traf: 1});
+                trafionePlayer1++;
+                trafiony = 1;
+                if(trafionePlayer1===14 || trafionePlayer2===14){
+                    if(trafionePlayer1===14){
+                        players[0].emit('end', 'koniec gry wygrales');
+                        players[1].emit('end', 'koniec gry wygral przeciwnik');
+                    }else{
+                        players[0].emit('end', 'koniec gry wygral przeciwnik');
+                        players[1].emit('end', 'koniec gry wygreles');
+                    }
+                }
+            }else{
+                trafiony = 0;
+            }
+            players[1].emit('odpShot', {'x' : msg.x, 'y' : msg.y, 'status' : 1, 'trafiony' : trafiony});
+        }else{
+            if(tab1[msg.x][msg.y]===1){
+                client.emit('dupa', {x: msg.x, y: msg.y, traf: 1});
+                trafionePlayer2++;
+                trafiony = 1;
+                if(trafionePlayer1===14 || trafionePlayer2===14){
+                    if(trafionePlayer1===14){
+                        players[0].emit('end', 'koniec gry wygrales');
+                        players[1].emit('end', 'koniec gry wygral przeciwnik');
+                    }else{
+                        players[0].emit('end', 'koniec gry wygral przeciwnik');
+                        players[1].emit('end', 'koniec gry wygreles');
+                    }
+                }
+            }else{
+                trafiony = 0;
+            }
+            players[0].emit('odpShot', {'x' : msg.x, 'y' : msg.y, 'status' : 1, 'trafiony' : trafiony});
+        }
+    });
+
+});
+
+server.listen(3030);
---
+++ skrypt.js
@@ -0,0 +1,108 @@
+/*jshint node: true, browser: true, jquery: true */
+/*global io: false */
+$(document).ready(function () {
+    'use strict';
+    var socket = io.connect('http://localhost:3030'),
+        myShip = [],
+        myShots = [],
+        status,
+        rivalNickname,
+        x,
+        y,
+        strzal;
+
+    console.log('connecting…');
+
+    socket.on('connect', function () {
+        console.log('connected!');
+    });
+
+    socket.on('nieGra', function (msg) {
+        var data = msg.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        $('body').text(data);
+    });
+
+    socket.on('waitForPlayer', function (msg){
+        var data = msg.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        $('#start').text(data);
+    });
+
+    socket.on('twojaTablica', function (msg){
+        console.log(msg);
+        myShip = msg.statki;
+        status = msg.status;
+        rivalNickname = msg.rival;
+        $('#start').hide();
+        for(var i=0; i<myShip.length; i++){
+            myShots[i] = new Array(8);
+            for(var j=0; j<myShip.length; j++){
+                if(myShip[i][j]===1){
+                    $('#myShip').append('<div id="'+i+'-'+j+'" style="background-color: red;"></div>');
+                } else{
+                    $('#myShip').append('<div id="'+i+'-'+j+'"></div>');    
+                }
+                $('#myShots').append('<div id="'+i+'-'+j+'"></div>');
+                myShots[i][j] = 0;
+            }
+        }
+        $('#myShip').show();
+        $('#myShots').show();
+
+        $('#myShots div').click(function (e){
+            if(status===0){
+                return ;
+            }else{
+                var pos = $(this).attr('id').split('-');
+                strzal = $(this);
+                x = pos[0];
+                y = pos[1];
+                console.log("x = " + x + ", y = " + y);
+                myShots[x][y] = 1;
+                socket.emit('shot', {'x' : x, 'y' : y});
+                $(this).css('background-color', 'yellow');
+                status = 0;
+            }
+        }); 
+
+
+    });
+
+    socket.on('dupa', function(msg){
+        console.log("x = " + msg.x + ", y = " + msg.y + ", traf = " + msg.traf);
+        var id = msg.x+"-"+msg.y;
+        $('#myShots div').each(function(value, key){
+            if($(this).attr('id')===id){
+                $(this).css('background-color', 'pink');
+            }
+        });
+    });
+
+    socket.on('end', function(msg){
+        $('#start').show();
+        $('#start').text(msg);
+        $('#myShip').hide();
+        $('#myShots').hide();
+    });
+
+    socket.on('odpShot', function (msg){
+        var id = '#' + msg.x + '-' + msg.y;
+        var idMyShip = '#myShip ' + id;
+        var idMyShot = '#myShots ' + id;
+        if(msg.trafiony===1){
+            myShip[msg.x][msg.y] = 3;
+            $(idMyShip).css('background-color', 'pink');
+        }else{
+            myShip[msg.x][msg.y] = 2;
+            $(idMyShip).css('background-color', 'green');
+        }
+        status = msg.status;
+    });
+
+    $('#nick').keypress(function (event){
+        if(event.keyCode===13){
+            socket.emit('setNick', $('#nick').val());
+        }
+    });
+
+    
+});
